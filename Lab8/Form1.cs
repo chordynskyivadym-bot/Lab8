4e41a337f435783f7e80c46d652b89586746db4c
Vadim <c.hordynskyi.vadym@student.uzhnu.edu.ua>
2026-03-15 15:17:21 +0200

    finish Part

--- Lab8/Form1.cs
+++ Lab8/Form1.cs
@@ -1,17 +1,37 @@
+using System.Diagnostics;
+
 namespace Lab8
 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
+        Stopwatch stopwatch = new Stopwatch();
+        int countClick = 0;
 
         private void button1_Click(object sender, EventArgs e) {
-            int maxCountClick = 10;
-            for (int i = 0; i < maxCountClick; i++) {
-                int RandomX = new Random().Next(75, 1205);
-                int RandomY = new Random().Next(50, 600);
-                button1.Location = new Point(RandomX, RandomY);
+            countClick++;
+
+            if (countClick >= 10) {
+                stopwatch.Stop();   
+                double averageTime = stopwatch.Elapsed.TotalSeconds / 10.0;
+                MessageBox.Show("Гра закінчена! Середній час реакції: " + Math.Round(averageTime, 3) + " секунд.");
+                countClick = 0;
+                stopwatch.Restart();
             }
+
+            MoveButtonToRandomLocation();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            MoveButtonToRandomLocation();
+            stopwatch.Start();
+        }
+        private void MoveButtonToRandomLocation() {
+            int X = new Random().Next(0, ClientSize.Width - button1.Width);
+            int Y = new Random().Next(0, ClientSize.Height - button1.Height);
+
+            button1.Location = new Point(X,Y);
         }
     }
 }
